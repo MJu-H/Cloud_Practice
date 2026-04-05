48b68df26f34df0ee08971e6e4755bec839ce307
MJu-H <monjurulhaque839@gmail.com>
2026-04-05 20:52:36 +1000

    Updated again

--- ftest1.py
+++ ftest1.py
@@ -2,6 +2,6 @@ I updates the wrong file before now I am changing again.
 
 Practice session 3 
 
-Lets talk about "Noyhing"...
-What is actually nathong means, is it actually nothing or a thing which we can
+Lets talk about "Nothing"...
+What is actually nothing means, is it actually nothing or a thing which we can
 not percive. 
